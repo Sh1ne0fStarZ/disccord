1c98a1b78df626edba4f01d99f13bd0d05e814d7
Cookie1355 <129585770+Cookie1355@users.noreply.github.com>
2026-05-28 13:33:44 +0300

    Initial commit

---
+++ src/main/java/com/sh1ne/disccord/Disccord.java
@@ -0,0 +1,15 @@
+package com.sh1ne.disccord;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Disccord implements ModInitializer {
+	public static final String MOD_ID = "disccord";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/sh1ne/disccord/DisccordDataGenerator.java
@@ -0,0 +1,11 @@
+package com.sh1ne.disccord;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DisccordDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
